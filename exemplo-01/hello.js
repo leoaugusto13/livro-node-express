c94dc4d68d54fd336cbca70e8a32945ede2811fe
Leonardo Augusto <leo.augustobh13@gmail.com>
2021-04-12 10:31:05 -0300

    Primeiro Commit

---
+++ exemplo-01/hello.js
@@ -0,0 +1,12 @@
+const http = require('http')
+const port = process.env.PORT || 3000
+
+/** Metódo http.createServer recebe uma função como argumento, essa função será chamada sempre que uma requisição
+ * HTTP for feita.
+ */
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end('Hello World!')
+})
+
+server.listen(port, () => console.log(`Server startrd on port ${port}; ` + 'press Ctrl-C to terminate.....'))
